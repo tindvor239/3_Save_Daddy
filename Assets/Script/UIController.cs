d9d9f74e17e8f5eece366936762f767e2f79b0c5
tindvor239 <37416837+tindvor239@users.noreply.github.com>
2021-05-07 09:30:50 +0700

    UI Controller

---
+++ Assets/Script/UIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoozyUI;
+
+public class UIController : Singleton<UIController>
+{
+    #region Singleton
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowUI()
+    {
+
+    }
+}
