fd4e3705c7c00993a8a0a758e006e6ce863fda0b
tindvor239 <37416837+tindvor239@users.noreply.github.com>
2021-05-07 10:38:08 +0700

    Update data

--- Assets/Script/Data/Map.cs
+++ Assets/Script/Data/Map.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.CustomComponents;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "New Map", menuName = "Data/Map", order = 1)]
 public class Map : Data
@@ -11,6 +12,10 @@ public class Map : Data
     public void Save()
     {
         packages = PackAllModels();
+        #if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+        #endif
         Debug.Log("Saved");
     }
     public void Load(List<PoolParty> poolParties)
