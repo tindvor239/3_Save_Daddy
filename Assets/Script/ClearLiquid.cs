fdf7ee2dd4c58149c72769979558f31caa12b24d
tindvor239 <37416837+tindvor239@users.noreply.github.com>
2021-05-14 00:05:41 +0700

    0

---
+++ Assets/Script/ClearLiquid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ClearLiquid : MonoBehaviour
+{
+    [SerializeField]
+    private LiquidPool liquidPoolParty;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Liquid")
+        {
+            bool canCreate = true;
+            foreach (GameObject gameObject in liquidPoolParty.Smokes.PooledObjects)
+            {
+                if (gameObject.activeInHierarchy == true)
+                {
+                    canCreate = false;
+                    break;
+                }
+            }
+            if(canCreate)
+            {
+                liquidPoolParty.CreateSmoke(collision.transform.position);
+            }
+            liquidPoolParty.Obstacles.GetBackToPool(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Traps/Fan.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : Trap
+{
+    [SerializeField]
+    private Transform target;
+    private float timer = 0;
+    private float maxTimer = 0.15f;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxTimer)
+        {
+            CheckHit();
+            timer = 0;
+        }
+    }
+
+    private void CheckHit()
+    {
+        GameObject beenHitObject = GameManager.RayCastToObject(transform.position, target.position);
+        if (beenHitObject != null)
+        {
+            OnHit(beenHitObject);
+        }
+    }
+
+    protected override void OnHit(GameObject beenHitObject)
+    {
+        
+    }
+}
